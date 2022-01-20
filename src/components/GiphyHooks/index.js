2f9cf5f953c2eb3788301ebe643cfc7eddd5a87a
Brendan Duffy <62387646+thewalkingduff@users.noreply.github.com>
2022-01-20 01:34:30 -0500

    clean up app.css and index.js

--- src/components/GiphyHooks/index.js
+++ src/components/GiphyHooks/index.js
@@ -34,14 +34,10 @@ const GifHooks = () => {
     }
 
     const handleSearchGifs = async () => {
-        // if (!gifInput) return;
 
         let api_key = '315qqaaVO6uwQ5uQD6ok0qsQqaAilA1NxE0Lj0e5'
 
-        // const res = await axios.get(`https://api.giphy.com/v1/gifs/search?&q=${gifInput}&api_key=${api_key}`)
         const res = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${api_key}`)
-        // const res = await axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${api_key}`)
-
         console.log('response: ', res)
 
         setGifs(res.data)
@@ -53,35 +49,31 @@ const GifHooks = () => {
 
             <div className='fav-gifs'>
 
-{savedGifs.map((gif, index) => {
-    console.log(gif)
-    return (
-        <div key={index} className="single-fav-gif">
-            {/* <img src={gif.images.fixed_width.url} alt=' ' /> */}
-            <h1>{gifs.title}</h1>
-            <img src={gifs.url} alt=' ' />
-            <p>{gifs.explanation}</p>
-            <button className="save-remove-button" onClick={() => handleRemoveGif(index)} >Remove</button>
+        {savedGifs.map((gif, index) => {
+        console.log(gif)
+        return (
+            <div key={index} className="single-fav-gif">
+                <h1>{gifs.title}</h1>
+                <img src={gifs.url} alt=' ' />
+                <p>{gifs.explanation}</p>
+                <button className="save-remove-button" onClick={() => handleRemoveGif(index)} >Remove</button>
+            </div>
+        )
+        })}
         </div>
-    )
-})}
-</div>
 
             <section className='search-section'>
-                {/* <h2>LOAD ASTRONOMY PICTURE OF THE DAY!</h2> */}
                 <button className="search-button" onClick={handleSearchGifs}>LOAD</button>
             </section>
 
 
             <div className='searched-gifs'> 
-        
                         <div className="single-search-gif">
                             <h1>{gifs.title}</h1>
                             <img src={gifs.url} alt=''/>
                             <p>{gifs.explanation}</p>
                             <button className="save-remove-button" onClick={() => handleSaveGif(gifs.url)}>Save</button>
                         </div>
-     
             </div>
         </div>
     );
